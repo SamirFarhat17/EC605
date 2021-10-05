9044a028417477695cd50897e8287dc79aa25b65
Samir Farhat Dominguez <47323704+SamirFarhat17@users.noreply.github.com>
2021-10-04 20:17:10 -0400

    Add files via upload

---
+++ wrap_around_leds/wrap_around_leds.srcs/sources_1/new/wrap_around_leds.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/04/2021 08:13:34 PM
+// Design Name: 
+// Module Name: wrap_around_leds
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wrap_around_leds(
+    input clk,
+    input rst,
+    input sw0,
+    input sw1,
+    input sw2,
+    input [7:0] cnt,
+    output led0,
+    output led1,
+    output led2,
+    output led3
+    );
+endmodule
